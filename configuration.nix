0fb9565997b33d5eeb79221b2540419efe61467c
Yuto Nishida <yuto@berkeley.edu>
2023-05-13 04:33:29 -0700

    Remove errors

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,8 @@
 
     # ssh
     services.openssh.enable = true;
-    services.openssh.permitRootLogin = "yes";
+    #services.openssh.permitRootLogin = "yes";
+    services.openssh.settings.PermitRootLogin = "yes";
     users.users.root.password = "nixos";
 
     security = {
@@ -21,7 +22,7 @@
         efiInstallAsRemovable = true;
     };
     fileSystems."/boot" = {
-        device = "/dev/vda1";  # /dev/disk/by-label/ESP
+        device = "/dev/disk/by-label/ESP";  # /dev/disk/by-label/ESP
         fsType = "vfat";
     };
     fileSystems."/" = {
@@ -33,9 +34,18 @@
 
     # misc
     boot.kernelPackages = pkgs.linuxPackages_latest;
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
     # pkgs
     environment.systemPackages = with pkgs; [
         vim
     ];
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "22.11"; # Did you read the comment?
 }
